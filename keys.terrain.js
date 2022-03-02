e2bf92803723addaa9f3e677a2894457ef5b1744
tomazmm <tomaz.mesarec@protonmail.com>
2022-03-02 12:55:43 +0100

    Added executes in lib

--- keys.terrain.js
+++ keys.terrain.js
@@ -2,7 +2,10 @@
 // to populate secret in CI environment instead of hardcoding
 
 module.exports = {
-  bombay: {
+  dev0: {
     mnemonic: "denial cinnamon muscle crash grid trend verify click hotel rail anxiety enjoy impulse you funny chicken weapon charge mountain wing roof border any kid"
   },
+  dev1: {
+    mnemonic: "edit equal sport surface sadness laundry weapon cheese devote street kiss link can ski fine sponsor together human margin suggest barely hungry pupil inmate"
+  }
 };
