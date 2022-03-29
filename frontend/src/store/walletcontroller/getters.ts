a63027ef4ed3f57aaa550a21832d08663e2e7d12
tomazmm <tomaz.mesarec@protonmail.com>
2022-03-29 18:47:38 +0200

    Removed frontend files

--- frontend/src/store/walletcontroller/getters.ts
+++
@@ -1,8 +0,0 @@
-import { GetterTree } from 'vuex';
-import { RootState, WalletControllerState } from '../types';
-
-const getters: GetterTree<WalletControllerState, RootState> = {
-  init: (state) => state.init,
-  instance: (state) => state.instance,
-};
-export default getters;
