edbf801cadac0c1231bf50e4dcea0631f9647c3f
Tomaž Mesarec <tomaz.mesarec@protonmail.com>
2022-04-02 15:18:07 +0200

    Adding modal view for a grid item

--- frontend/src/components/layout/MyAccount/MyAccountsContent/GridItem.tsx
+++ frontend/src/components/layout/MyAccount/MyAccountsContent/GridItem.tsx
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from "react";
 import styled from 'styled-components';
-import {Col, Container, Row, Spinner} from "react-bootstrap";
+import {Col, Modal, Button } from "react-bootstrap";
 
 interface GridItemProps {
   className?: string;
@@ -15,22 +15,39 @@ export const GridItemBase = (props: GridItemProps) => {
 
 
   const [imageName, setImageName] = useState<any>("")
+  const [show, setShow] = useState(false);
 
   useEffect(() => {
     const tempImageName = nftValue.extension.image.split("//");
     setImageName("https://ipfs.io/ipfs/" + tempImageName[1]);
   }, []);
 
-  const showModal = (e: any) => {
-    console.log("Pop up modal");
+  const toggleModal = (e: any) => {
+    setShow(!show);
   }
 
   return (
-    <div className={className} onClick={showModal}>
+    <div className={className} onClick={toggleModal}>
       <div className="grid-item-wrapper d-flex flex-column">
         <img src={imageName} />
         <span>{nftValue.extension.name}</span>
       </div>
+
+      <Modal show={show} size="lg">
+        <Modal.Header closeButton>
+          <Modal.Title># {nftValue.extension.name}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <div className="modal-body-inner d-flex flex-row">
+
+          </div>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={toggleModal}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </div>
   )
 }
@@ -39,6 +56,7 @@ export const GridItemBase = (props: GridItemProps) => {
 export const GridItem = styled(GridItemBase)`
   background-color: rgba(33, 30, 26, 0.7);
   .grid-item-wrapper {
+    cursor: pointer;
     img{
       max-width: 100%;
       max-height: 100%;
@@ -48,6 +66,12 @@ export const GridItem = styled(GridItemBase)`
       color: white;
       padding-bottom: 2rem ;
       padding-left: 1rem;
+    }
+  }
+  
+  .modal-content{
+    img{
+      display: none;
     }
   }
 `;
