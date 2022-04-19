abfc843dbb0a0c71ff8712b5dd1eba4b432e40aa
Blaz Mesarec <blaz.mesarec@student.tugraz.at>
2022-04-19 18:07:53 +0200

    navigation added to burger menu

--- frontend/src/components/layout/Header/components/BurgerMenu.tsx
+++ frontend/src/components/layout/Header/components/BurgerMenu.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import styled from 'styled-components';
 import {Col, Container, Modal, Row,} from "react-bootstrap";
 import {CollectionMenu} from "../../../../pages/my-profile/components/CollectionMenu";
+import {ConnectWallet} from "../../../shared/ConnectWallet";
+import {useLocation, useNavigate} from "react-router-dom";
 
 
 interface BurgerMenuProps {
@@ -15,17 +17,34 @@ export const BurgerMenuBase = (props: BurgerMenuProps) => {
     show
   } = props;
 
+  const navigate = useNavigate();
+  const navigateMyProfile = () => navigate('/my-profile');
+
+  const location = useLocation();
+
   return (
     <div className={className}>
       <Container fluid className="h-100">
         <Row className="h-auto about my-4">
-          <Col
-               xs={{span: 12}}
-               className="d-flex flex-column justify-content-center align-self-start align-items-center col my-collection">
-            <div className="white-line"></div>
-            <CollectionMenu/>
+          <Col xs={{span: 12}} className="col navbar-burger">
+            <span onClick={navigateMyProfile} className="my-profile">My profile</span>
+            <ConnectWallet />
           </Col>
         </Row>
+        { location.pathname === "/my-profile"
+          ?
+            <Row className="h-auto about my-4">
+              <Col
+                  xs={{span: 12}}
+                  className="d-flex flex-column justify-content-center align-self-start align-items-center col my-collection">
+                <div className="white-line"></div>
+                <CollectionMenu/>
+              </Col>
+            </Row>
+          :
+            <></>
+        }
+
       </Container>
     </div>
   )
@@ -46,5 +65,24 @@ export const BurgerMenu = styled(BurgerMenuBase)`
   background-color: rgba(0,0,0,1);
   z-index: 1000;
   width: 100%;
-  margin-top: 7rem;
+  padding: 1rem;
+  margin-top: 6rem;
+  .container-fluid{
+    .row{
+      .col{
+        &.navbar-burger{
+          display: flex;
+          justify-content: end;
+          .my-profile{
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            padding-right: 1rem;
+            color: white;
+            cursor: pointer;
+          }
+        }
+      }
+    }
+  }
 `;
