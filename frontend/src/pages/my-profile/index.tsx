ddb1ff9358eafdfc665c9030ce6230a265db55dc
tomazmm <tomaz.mesarec@protonmail.com>
2022-04-27 17:36:20 +0200

    Styled collection grid

--- frontend/src/pages/my-profile/index.tsx
+++ frontend/src/pages/my-profile/index.tsx
@@ -18,24 +18,16 @@ export const MyProfileBase = (props: MyProfileProps) => {
     <div className={`${className}`} >
       <Container fluid >
           <Row>
-            <Col xl={{span: 3}}
-                 lg={{span: 12}}
-                 xs={{span: 12}}
-                 className="d-flex flex-column justify-content-center align-self-start align-items-center col my-collection">
-              <span className="owned-text">My Collection</span>
-              <div className="white-line"></div>
-              <CollectionMenu/>
-            </Col>
-            <Col xl={{span: 9}}
+            <Col xl={{span: 12}}
                  lg={{span: 12}}
                  xs={{span: 12}}
                  className="d-flex flex-column col my-account-col">
-              <div className="white-line nfts"></div>
-              <Grid/>
+                <h2 className="mb-0 text-white text-bold">Collection</h2>
+                <div className="white-line nfts"></div>
+                <Grid/>
             </Col>
           </Row>
       </Container>
-
     </div>
   )
 }
@@ -43,45 +35,25 @@ export const MyProfileBase = (props: MyProfileProps) => {
 
 export const MyProfile= styled(MyProfileBase)`
   height: 100%;
+  margin-top: 2.5em;
     > .container-fluid{
         height: 100%;
-        width: 95%;
+        width: 70%;
         .row{
           height: 100%;
           .col{
-            &.my-collection{
-              height: 100%;
-              @media screen and (max-width: 767px) {
-                display: none!important;
-              }
-              @media screen and (max-width: 1200px) and (min-width: 768px){
-                height: 20%;
-              }
-            }
             &.my-account-col{
               height: 95%;
               @media screen and (max-width: 1200px) and (min-width: 768px){
                 height: 70%;
               }
             }
-            .owned-text{
-              color: white;
-              font-size: 1.7rem;
-              font-weight: 700;
-              margin-bottom: 0.5rem;
-            }
-            .my-account-text{
-              color: white;
-              font-size: 3rem;
-              font-weight: 700;
-              margin-bottom: 1rem;
-            }
             .white-line{
-              border-bottom: 2px groove rgba(194, 194, 194, .8);
+              border-bottom: 2px groove white;
               width: 100%;
               margin-bottom: 2rem;
               &.nfts{
-                margin-top: 3.07rem;
+                margin-top: 1rem;
                 @media screen and (max-width: 1200px) and (min-width: 768px){
                   margin-top: 0;
                 }
