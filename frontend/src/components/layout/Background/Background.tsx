fb2ba9569e5a98b029aeb64f37482fd403768b54
tomazmm <tomaz.mesarec@protonmail.com>
2022-05-04 16:12:48 +0200

    Tier selection

--- frontend/src/components/layout/Background/Background.tsx
+++ frontend/src/components/layout/Background/Background.tsx
@@ -15,14 +15,13 @@ export const BackgroundBase = (props: BackgroundProps) => {
     )
 }
 
-
 export const Background = styled(BackgroundBase)`
   position: absolute;
   top: 0;
   height: 100%;
   width: 100%;
   z-index: -1;
-  //background: url("/assets/background_test_2.jpg");
+  //background: url("/assets/space-background.png");
   background: rgba(187,187,187, .1);
   background-size: cover;
   opacity: 0.4;
